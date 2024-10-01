ae6d92507ef6e5c51b106161b1137e2ed3cce6c3
Esteban Montoya <emontoyab93@gmail.com>
2024-10-01 00:24:16 -0500

    initial commit

---
+++ 01 - Backend using SpringBoot and SQL Database/a) cv-tbn/src/main/java/com/nodo/cv_tbn/CvTbnApplication.java
@@ -0,0 +1,13 @@
+package com.nodo.cv_tbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvTbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvTbnApplication.class, args);
+	}
+
+}
